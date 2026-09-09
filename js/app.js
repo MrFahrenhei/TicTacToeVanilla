11e17b881fa754bafb31b91f84f6fc964469c157
Beraldo <vvberaldo@proton.me>
2026-09-08 22:15:02 -0300

    now you click in a square and the game decides if is X or O

--- js/app.js
+++ js/app.js
@@ -15,14 +15,23 @@
 const App = {
     // all of our selected html elements
     $: {
-        menu: document.querySelector("[data-id=menu]"),
-        menuItems: document.querySelector("[data-id=menu-items]"),
-        resetBtn: document.querySelector("[data-id=reset-btn]"),
-        newRoundBtn: document.querySelector("[data-id=new-round-btn]"),
+        menu: document.querySelector('[data-id="menu"]'),
+        menuItems: document.querySelector('[data-id="menu-items"]'),
+        resetBtn: document.querySelector('[data-id="reset-btn"]'),
+        newRoundBtn: document.querySelector('[data-id="new-round-btn"]'),
+        squares: document.querySelectorAll('[data-id="square"]'),
+    },
+
+    state:{
+        currentPlayer: 1,
     },
     // anonymous function
     // init: () => {}
     init() {
+        App.registerEventListeners();
+        
+    },
+    registerEventListeners(){
         App.$.menu.addEventListener('click', (event)=>{
             App.$.menuItems.classList.toggle('hidden');
         });
@@ -34,7 +43,24 @@ const App = {
         App.$.newRoundBtn.addEventListener('click', event => {
             console.log("Add a new round");
         });
-    },
+
+        App.$.squares.forEach((square)=> {
+            square.addEventListener('click', event=>{
+                if(square.hasChildNodes()){return;}
+                const currentPlayer = App.state.currentPlayer;
+                const icon = document.createElement("i");
+                if(currentPlayer === 1){
+                    icon.classList.add('fa-solid', 'fa-x', 'yellow');
+                }else{
+                    icon.classList.add('fa-solid', 'fa-o', 'green');
+                }
+                App.state.currentPlayer = App.state.currentPlayer === 1 ? 2 : 1;
+                square.replaceChildren(icon);
+
+            });
+        });
+    }
+    
 }
 
 window.addEventListener('load', App.init);
