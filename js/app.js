e11061cfc7201acf494601f3150329436e606f4f
Beraldo <vvberaldo@proton.me>
2026-09-09 20:46:37 -0300

    logic for winner or tie is complete

--- js/app.js
+++ js/app.js
@@ -24,9 +24,35 @@ const App = {
 
     state:{
         currentPlayer: 1,
+        moves: []
     },
-    // anonymous function
-    // init: () => {}
+    getGameStatus(moves){
+        const p1Moves = moves.filter(move => move.playerId==1).map(move => +move.squareId);
+        const p2Moves = moves.filter(move => move.playerId==2).map(move => +move.squareId);
+        // check if there is a winner or tie game
+        const winningPatterns = [
+            [1, 2, 3],
+            [1, 5, 9],
+            [1, 4, 7],
+            [2, 5, 8],
+            [3, 5, 7],
+            [3, 6, 9],
+            [4, 5, 6],
+            [7, 8, 9],
+        ];
+        let winner = null;
+        winningPatterns.forEach(pattern => {
+            const p1Wins = pattern.every(v => p1Moves.includes(v));
+            const p2Wins = pattern.every(v => p2Moves.includes(v));
+
+            if(p1Wins) winner = 1
+            if(p2Wins) winner = 2
+        })
+        return{
+            status: moves.length === 9|| winner != null ? 'complete' :  'in-progress',
+            winner
+        }
+    },
     init() {
         App.registerEventListeners();
         
@@ -46,17 +72,50 @@ const App = {
 
         App.$.squares.forEach((square)=> {
             square.addEventListener('click', event=>{
-                if(square.hasChildNodes()){return;}
-                const currentPlayer = App.state.currentPlayer;
+                const hasMove = (squareId)=>{
+                    const existingMove = App.state.moves.find(move=>move.squareId === squareId);
+                    return existingMove !== undefined;
+                }
+
+                if(hasMove(+square.id)){
+                    return;
+                }
+               
+                // Check if there is already a play
+                // if(square.hasChildNodes()){return;}
+
+                // determine which player icon to add to the square
+                //const currentPlayer = App.state.currentPlayer;
+
+                const lastMove = App.state.moves.at(-1);
+                const getOppositePlayer = (playerId) => playerId === 1 ? 2 : 1
+                const currentPlayer =
+                      App.state.moves.length === 0
+                      ? 1
+                      : getOppositePlayer(lastMove.playerId);
+                
                 const icon = document.createElement("i");
                 if(currentPlayer === 1){
                     icon.classList.add('fa-solid', 'fa-x', 'yellow');
                 }else{
                     icon.classList.add('fa-solid', 'fa-o', 'green');
                 }
-                App.state.currentPlayer = App.state.currentPlayer === 1 ? 2 : 1;
+                App.state.moves.push({
+                    squareId: +square.id,
+                    playerId: currentPlayer
+                });
+                App.state.currentPlayer = currentPlayer === 1 ? 2 : 1;
+
                 square.replaceChildren(icon);
 
+                const game = App.getGameStatus(App.state.moves);
+                if(game.status === 'complete'){
+                    if(game.winner){
+                        alert(`Player ${game.winner} wins!`);
+                    }else{
+                        alert("Tie!");
+                    }
+                }
             });
         });
     }
